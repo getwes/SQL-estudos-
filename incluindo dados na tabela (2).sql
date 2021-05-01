b95979a7d27cec7d46417f843dde88e908e6e89b
getwes <wesleyfreire1707@gmail.com>
2021-05-01 02:02:18 -0300

    incluindo os dados da tabela

---
+++ incluindo dados na tabela (2).sql
@@ -0,0 +1,4 @@
+insert into [tabela de vendedores]
+([matricula], [nome], [percentual comissão])
+values
+('00233', 'jose geraldo da fonseca', 0.10)
---
+++ incluindo dados na tabela.sql
@@ -0,0 +1,12 @@
+USE [SUCOS_VENDAS]
+
+
+--insert into [dbo].[tabela de produtos]
+
+--([codigo do produto], [nome do produto], [embalagem], [tamanho], [sabor], [preço de lista])
+
+--values
+
+--('1040107','light - 350 ml - melancia', 'lata', '350 ml', 'melancia', 4.56)
+
+select * from [dbo].[tabela de produtos];
