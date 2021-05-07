84135a9f94fabb00ee7b609882c50e2ff82f3d77
getwes <wesleyfreire1707@gmail.com>
2021-05-06 21:23:07 -0300

    atualização 06/05/2021

---
+++ exercicio para selecionar nome e matricula dos vendedores.sql
@@ -0,0 +1,13 @@
+
+
+select * from [tabela de produtos]
+
+delete from [tabela de produtos] where [codigo do porduto]='1000889'
+
+select * from [tabela de produtos] where [codigo do produto]='1000889'
+
+select * from [tabela de produtos] where [sabor]='limão'
+
+-- com o where tem como apagar varias linhas com o mesmo nome na coluna 
+
+delete from [tabela de produtos] where [sabor]='limão'
---
+++ filtragem de registros.sql
@@ -0,0 +1,11 @@
+/****** Script do comando SelectTopNRows de SSMS  ******/
+SELECT TOP (1000) [matricula]
+      ,[nome]
+      ,[percentual comissão]
+  FROM [SUCOS_VENDAS].[dbo].[tabela de vendedores]
+
+  --SELECT [nome]
+  --    ,[matricula]
+	 -- from [tabela de vendedores]
+
+	  
